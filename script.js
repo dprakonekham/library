a4d5071bcac43d17bbd8659f2910e65b944e17b4
Danny <dprakonekham@gmail.com>
2023-03-11 01:25:18 -0800

    made the add button function, will accept user input later

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
 let myLibrary = [];
 
+function localSave(){
+    //Load existing library
+}
+
+function initialPrint(){
+    //Iterate myLibrary objects and add to the library container
+    Object.keys(myLibrary).forEach(function(book){
+        const library = document.getElementById("libraryContainer");
+
+        const bookContainer = document.createElement("book" + book.toString);
+        bookContainer.style.display = "flex";
+        bookContainer.style.flexDirection = "column";
+        bookContainer.style.alignContent = "center";
+        bookContainer.style.justifyContent = "center";
+        bookContainer.style.color = "purple";
+        bookContainer.style.backgroundColor = "gold";
+        bookContainer.style.border = "solid black";
+
+        const newTitle = document.createElement("title" + book.toString);
+        newTitle.textContent = myLibrary[book].title;
+        bookContainer.appendChild(newTitle);
+
+        const newAuthor = document.createElement("author" + book.toString);
+        newAuthor.textContent = myLibrary[book].author;
+        bookContainer.appendChild(newAuthor);
+
+        library.appendChild(bookContainer)
+    });
+}
+
 function Book(title,author,pages,currentPage){
     this.title = title;
     this.author = author;
@@ -10,4 +40,55 @@ function Book(title,author,pages,currentPage){
 function addBooktoLibrary(book){
     myLibrary.push(book);
 }
+
+const addButton = document.getElementById("addBookBtn");
+addButton.addEventListener('click', function(e){
+    
+    const newBook = new Book("Game of Thrones","George RR Martin", 112, 1);
+    myLibrary.push(newBook);
+    
+    const library = document.getElementById("libraryContainer");
+    const bookContainer = document.createElement("book" + (myLibrary.length-1).toString());
+    bookContainer.style.display = "flex";
+    bookContainer.style.flexDirection = "column";
+    bookContainer.style.alignContent = "center";
+    bookContainer.style.justifyContent = "center";
+    bookContainer.style.color = "purple";
+    bookContainer.style.backgroundColor = "gold";
+    bookContainer.style.border = "solid black";
+
+    const newTitle = document.createElement("title" + (myLibrary.length-1).toString());
+    bookContainer.style.display = "flex";
+    newTitle.textContent = newBook.title;
+    bookContainer.appendChild(newTitle);
+
+    const newAuthor = document.createElement("author" + (myLibrary.length-1).toString());
+    bookContainer.style.display = "flex";
+    newAuthor.textContent = newBook.author;
+    bookContainer.appendChild(newAuthor);
+
+    const button = document.createElement("button");
+    button.textContent = "Delete";
+    bookContainer.appendChild(button);
+    button.addEventListener('click',function(e){
+        console.log(e.target.parentNode)
+    })
+
+    library.appendChild(bookContainer)
+});
+
+function deleteBook(){
+    //Get array location of book
+    //Delete and shift array
+    
+}
+
+function openForm(){
+    document.querySelector("addBookForm").style.display = "block"
+}
+
+function closeForm(){
+    document.querySelector("addBookForm").style.display = "none"
+}
+
 
