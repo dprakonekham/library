33766c021f20ba8ce9c30fea5f0df62a59995c0a
Danny <dprakonekham@gmail.com>
2023-03-14 01:48:34 -0700

    made it so every field except the current page must be filled in for the add button to become enabled

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let myLibrary = [];
 
+const bookForm = document.getElementById("addBookForm")
+const library = document.getElementById("libraryContainer");
+
 function localSave(){
     //Load existing library
 }
@@ -7,8 +10,6 @@ function localSave(){
 function initialPrint(){
     //Iterate myLibrary objects and add to the library container
     Object.keys(myLibrary).forEach(function(book){
-        const library = document.getElementById("libraryContainer");
-
         const bookContainer = document.createElement("book" + book.toString);
         bookContainer.style.display = "flex";
         bookContainer.style.flexDirection = "column";
@@ -25,8 +26,6 @@ function initialPrint(){
         const newAuthor = document.createElement("author" + book.toString);
         newAuthor.textContent = myLibrary[book].author;
         bookContainer.appendChild(newAuthor);
-
-        library.appendChild(bookContainer)
     });
 }
 
@@ -43,9 +42,9 @@ function addBooktoLibrary(book){
 
 const addButton = document.getElementById("addBookBtn");
 addButton.addEventListener('click', function(e){
-    if(document.getElementById("addBookForm").style.display != "block"){
+    if(bookForm.style.display != "block"){
         openForm();
-    }else if (document.getElementById("addBookForm").style.display == "block"){
+    }else if (bookForm.style.display == "block"){
         closeForm();
     }
 });
@@ -61,71 +60,88 @@ function deleteBook(parentID){
 }
 
 function openForm(){
-    document.getElementById("addBookForm").style.display = "block"
+    bookForm.style.display = "block"
 }
 
 function closeForm(){
-    document.getElementById("addBookForm").style.display = "none"
+    bookForm.reset();
+    document.getElementById("add").disabled = true;
+    bookForm.style.display = "none"
 }
-
-function getFormContents(form){
-    console.log(form.elements[3].value)
-    console.log(form.elements[4].value)
-    let formComplete = false;
-    //while form complete flag is not true
-    while(formComplete == false){
-        if(form.elements[1].value == ""){
-            
-        }else if(form.elements[2].value == ""){
-            
-        }else if(form.elements[3].value == ""){
-
-        }else{
-            if(form.elements[4].value == ""){
-                form.elements[4].value = 0
-            }
-            const newBook = new Book(form.elements[1].value,form.elements[2].value, form.elements[3].value, form.elements[4].value);
-            myLibrary.push(newBook);
-        
-            const library = document.getElementById("libraryContainer");
-            const bookContainer = document.createElement("book" + (myLibrary.length-1).toString());
-            bookContainer.id = "book" + (myLibrary.length-1).toString();
-            bookContainer.style.display = "flex";
-            bookContainer.style.flexDirection = "column";
-            bookContainer.style.alignContent = "center";
-            bookContainer.style.justifyContent = "center";
-            bookContainer.style.color = "purple";
-            bookContainer.style.backgroundColor = "gold";
-            bookContainer.style.border = "solid black";
-    
-            const newTitle = document.createElement("title" + (myLibrary.length-1).toString());
-            newTitle.textContent = newBook.title;
-            bookContainer.appendChild(newTitle);
-    
-            const newAuthor = document.createElement("author" + (myLibrary.length-1).toString());
-            newAuthor.textContent = newBook.author;
-            bookContainer.appendChild(newAuthor);
-
-            const newPages= document.createElement("pages" + (myLibrary.length-1).toString());
-            newPages.textContent = newBook.pages;
-            bookContainer.appendChild(newPages);
-
-            const newCurrentPage = document.createElement("cPages" + (myLibrary.length-1).toString());
-            newCurrentPage.textContent = newBook.currentPage;
-            bookContainer.appendChild(newCurrentPage);
-    
-            const button = document.createElement("button");
-            button.textContent = "Delete";
-            bookContainer.appendChild(button);
-            button.addEventListener('click',function(e){
-            //Get id of the parent node of the clicked button
-            deleteBook(e.target.parentNode.id);
-            })
-            library.appendChild(bookContainer)
-            document.getElementById("addBookForm").reset();
-            formComplete = true;
-        }
-    }
+const closeButton = document.getElementById("closeBtn")
+closeButton.addEventListener(`click`, function(e){
     closeForm();
+})
+function getFormContents(form){
+    if(form.elements[4].value == ""){
+        form.elements[4].value = 0
+    }
+        const newBook = new Book(form.elements[1].value,form.elements[2].value, form.elements[3].value, form.elements[4].value);
+        myLibrary.push(newBook);
+        
+        const bookContainer = document.createElement("book" + (myLibrary.length-1).toString());
+        bookContainer.id = "book" + (myLibrary.length-1).toString();
+        bookContainer.style.display = "flex";
+        bookContainer.style.flexDirection = "column";
+        bookContainer.style.alignContent = "center";
+        bookContainer.style.justifyContent = "center";
+        bookContainer.style.color = "purple";
+        bookContainer.style.backgroundColor = "gold";
+        bookContainer.style.border = "solid black";
+    
+        const newTitle = document.createElement("title" + (myLibrary.length-1).toString());
+        newTitle.textContent = newBook.title;
+        bookContainer.appendChild(newTitle);
+    
+        const newAuthor = document.createElement("author" + (myLibrary.length-1).toString());
+        newAuthor.textContent = newBook.author;
+        bookContainer.appendChild(newAuthor);
+
+        const newPages= document.createElement("pages" + (myLibrary.length-1).toString());
+        newPages.textContent = newBook.pages;
+        bookContainer.appendChild(newPages);
+
+        const newCurrentPage = document.createElement("cPages" + (myLibrary.length-1).toString());
+        newCurrentPage.textContent = newBook.currentPage;
+        bookContainer.appendChild(newCurrentPage);
+    
+        const button = document.createElement("button");
+        button.textContent = "Delete";
+        bookContainer.appendChild(button);
+        button.addEventListener('click',function(e){
+        //Get id of the parent node of the clicked button
+        deleteBook(e.target.parentNode.id);
+        })
+        library.appendChild(bookContainer)
+        closeForm();         
 }
+
+let titleInput = document.getElementById("bookTitleInput");
+let authorInput = document.getElementById("bookAuthorInput");
+let pagesInput = document.getElementById("bookPagesInput");
+
+titleInput.addEventListener(`keyup`, function(e){
+    if(titleInput.value != "" && authorInput.value != "" && pagesInput.value != ""){
+        document.getElementById("add").disabled = false;
+    }else{
+        document.getElementById("add").disabled = true;
+    }
+})
+
+authorInput.addEventListener(`keyup`, function(e){
+    if(titleInput.value != "" && authorInput.value != "" && pagesInput.value != ""){
+        document.getElementById("add").disabled = false;
+    }else{
+        document.getElementById("add").disabled = true;
+    }
+})
+
+pagesInput.addEventListener(`keyup`, function(e){
+    if(titleInput.value != "" && authorInput.value != "" && pagesInput.value != ""){
+        document.getElementById("add").disabled = false;
+    }else{
+        document.getElementById("add").disabled = true;
+    }
+})
+
 
